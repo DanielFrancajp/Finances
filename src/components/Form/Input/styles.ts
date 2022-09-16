9eeeffdab44f969061448a6ddedf51bbd180acb6
Daniel de França <danieldenotas@gmail.com>
2022-09-16 10:18:41 -0300

    fixed error styled-conponents

--- src/components/Form/Input/styles.ts
+++ src/components/Form/Input/styles.ts
@@ -3,7 +3,7 @@ import { TextInput } from "react-native";
 import { RFValue } from "react-native-responsive-fontsize";
 
 export const Container = styled(TextInput)`
-    width: 100 %;
+    width: '100 %';
     padding: 16px 18px;
 
     font-family: ${({ theme }) => theme.fonts.regular};
