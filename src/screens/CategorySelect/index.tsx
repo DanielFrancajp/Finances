bba49bd328b97cd1f34f96ee28797bb0eb489520
Daniel de França <danieldenotas@gmail.com>
2022-09-21 23:38:33 -0300

    finalização da pasta category

--- src/screens/CategorySelect/index.tsx
+++ src/screens/CategorySelect/index.tsx
@@ -1,8 +1,17 @@
 import React from "react";
-import { 
+import { FlatList } from "react-native";
+import { Button } from '../../components/Form/Button'
+import { categories } from "../../utils/categories";
+import {
     Container,
     Header,
     Title,
+    Category,
+    Icon,
+    Name,
+    Separator,
+    Footer,
+
 
 } from "./styles";
 
@@ -29,6 +38,30 @@ export function CategorySelect({
                     Categoria
                 </Title>
             </Header>
+
+            <FlatList
+                data={categories}
+                style={{ flex: 1, width: "100 %" }}
+                keyExtractor={(item) => item.key}
+                renderItem={({ item }) => (
+                    <Category>
+                        <Icon name={item.icon} />
+                        <Name>
+                            {item.name}
+                        </Name>
+                    </Category>
+                )}
+
+                ItemSeparatorComponent={() => <Separator />}
+
+
+            />
+
+            <Footer>
+                <Button title={"Selecionar"}/>
+                 
+            </Footer>
+
         </Container>
     )
 }
